24fabe570914a82cdd2de852b24b9a018e9c1c9a
Raffaello Bertini <raffaellobertini@gmail.com>
2022-04-13 22:36:24 +0100

    check-version checking also published packages

--- check-versions.ps1
+++ check-versions.ps1
@@ -1,16 +1,18 @@
 ###
 # Unnamed Arguments:
-# - Args[0] = $(Build.SourceVersion)
-# - Args[0] eg => refs/tags/v0.1.0
+# - $Args[0] = $(Build.SourceVersion)
+# - $Args[0] eg => refs/tags/v0.1.0
 
 ###
 
 $ErrorActionPreference = "Stop"
+echo $Args[0]
 
 ### if doesn't found the branch will return null, that will generate an error and exit.
-if (git branch --contains Arg[0] | Select-String -Pattern 'main$') {}
+$tagBranch = git branch --contains $Args[0]
+if ($tagBranch | Select-String -Pattern 'line$') {}
 else {
-    echo "git tag not in main branch"
+    echo "git tag not in main branch, but in $tagBranch"
     exit 1
 }
 
@@ -32,6 +34,7 @@ $v2 = Select-String -Path .\PowerDir\PowerDir.csproj -Pattern "<Version>(\d).(\d
     }
 }
 
+echo "checking version Manifest <-> Project ..."
 if ($v1.Major -eq $v2.Major -and $v1.Minor -eq $v2.Minor -and $v1.Patch -eq $v2.Patch) {}
 else {
     echo "Versions mismatching"
@@ -42,8 +45,9 @@ else {
     exit 1
 }
 
-$v3 = git describe --tags --exact-match Args[0]
-$v3 | Select-String -Pattern '^v(\d).(\d).(\d)$' | ForEach-Object {
+echo "checking version Manifest <-> git tag ..."
+$tag = git describe --tags --exact-match $Args[0]
+$v3 = $tag | Select-String -Pattern '^v(\d).(\d).(\d)$' | ForEach-Object {
     $major,$minor,$patch = $_.Matches[0].Groups[1..3].Value
     [PSCustomObject] @{
       Major = $major
@@ -61,3 +65,25 @@ else {
     $v3
     exit 1
 }
+
+echo "check published modules..."
+if (Find-Module -name PowerDir.GetPowerDir -ErrorAction SilentlyContinue) {
+    echo "version already existing, exit!"
+    exit 1
+}
+
+echo "version not found OK!"
+
+# $f = Find-Module -name PowerDir.GetPowerDir | Select Version
+# $v4 = [version]$f.Version
+# if ($v1.Major -eq $v3.Major -and $v1.Minor -eq $v3.Minor -and $v1.Patch -eq $v3.Patch) {}
+# else {
+#     echo "Versions mismatching"
+#     echo "psd1 Manifest file"
+#     $v1
+#     echo "git tag"
+#     $v3
+#     exit 1
+# }
+
+echo "OK"
