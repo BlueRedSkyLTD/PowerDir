48049efc833f5c4d87a2f4130f1b4436d136c55d
Raffaello Bertini <raffaellobertini@gmail.com>
2022-04-13 22:56:14 +0100

    check-versions in CI too

--- check-versions.ps1
+++ check-versions.ps1
@@ -1,90 +1,36 @@
 ###
-# Unnamed Arguments:
-# - $Args[0] = $(Build.SourceVersion)
-# - $Args[0] eg => refs/tags/v0.1.0
-
+# It checks the project version that are matching
+# and the version is not already published
 ###
 
 $ErrorActionPreference = "Stop"
-echo $Args[0]
 
-### if doesn't found the branch will return null, that will generate an error and exit.
-$tagBranch = git branch --contains $Args[0]
-if ($tagBranch | Select-String -Pattern 'line$') {}
-else {
-    echo "git tag not in main branch, but in $tagBranch"
-    exit 1
-}
+$Result = 0
 
-### TODO: instead of [PSCustomOjbect] use [Version]
 $v1 = Select-String -Path .\PowerDir\PowerDir.GetPowerDir.psd1 -Pattern "^ModuleVersion = '(\d).(\d).(\d)'$" | ForEach-Object {
     $major, $minor, $patch = $_.Matches[0].Groups[1..3].Value
-    [PSCustomObject] @{
-      Major = $major
-      Minor = $minor
-      Patch = $patch
-    }
+    [Version] "$major.$minor.$patch"
 }
 
 $v2 = Select-String -Path .\PowerDir\PowerDir.csproj -Pattern "<Version>(\d).(\d).(\d)" | ForEach-Object {
     $major,$minor,$patch = $_.Matches[0].Groups[1..3].Value
-    [PSCustomObject] @{
-        Major = $major
-        Minor = $minor
-        Patch = $patch
-    }
+    [Version] "$major.$minor.$patch"
 }
 
 echo "checking version Manifest <-> Project ..."
-if ($v1.Major -eq $v2.Major -and $v1.Minor -eq $v2.Minor -and $v1.Patch -eq $v2.Patch) {}
-else {
+if ($v1 -ne $v2) {
     echo "Versions mismatching"
     echo "psd1 Manifest file"
     $v1
     echo "C# project version"
     $v2
-    exit 1
-}
-
-echo "checking version Manifest <-> git tag ..."
-$tag = git describe --tags --exact-match $Args[0]
-$v3 = $tag | Select-String -Pattern '^v(\d).(\d).(\d)$' | ForEach-Object {
-    $major,$minor,$patch = $_.Matches[0].Groups[1..3].Value
-    [PSCustomObject] @{
-      Major = $major
-      Minor =$minor
-      Patch=$patch
-    }
-}
-
-if ($v1.Major -eq $v3.Major -and $v1.Minor -eq $v3.Minor -and $v1.Patch -eq $v3.Patch) {}
-else {
-    echo "Versions mismatching"
-    echo "psd1 Manifest file"
-    $v1
-    echo "git tag"
-    $v3
-    exit 1
+    $Result++
 }
 
 echo "check published modules..."
-if (Find-Module -name PowerDir.GetPowerDir -RequiredVersion $tag.Substring(1) -ErrorAction SilentlyContinue) {
-    echo "version already existing, exit!"
-    exit 1
+if (Find-Module -name PowerDir.GetPowerDir -RequiredVersion $v1.toString() -ErrorAction SilentlyContinue) {
+    echo "version already existing"
+    $Result++
 }
 
-echo "version not found OK!"
-
-# $f = Find-Module -name PowerDir.GetPowerDir | Select Version
-# $v4 = [version]$f.Version
-# if ($v1.Major -eq $v3.Major -and $v1.Minor -eq $v3.Minor -and $v1.Patch -eq $v3.Patch) {}
-# else {
-#     echo "Versions mismatching"
-#     echo "psd1 Manifest file"
-#     $v1
-#     echo "git tag"
-#     $v3
-#     exit 1
-# }
-
-echo "OK"
+exit $Result
