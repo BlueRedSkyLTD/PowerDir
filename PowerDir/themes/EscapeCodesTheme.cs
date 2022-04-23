1223411626433c4bcd32f73f5696447f4beda4e1
Raffaello Bertini <raffaellobertini@gmail.com>
2022-04-23 23:34:29 +0100

    [WIP] experimenting with HSV mixing colors

--- PowerDir/themes/EscapeCodesTheme.cs
+++ PowerDir/themes/EscapeCodesTheme.cs
@@ -65,6 +65,106 @@ namespace PowerDir.themes
             { KeyColorTheme.READONLY_FILE, new ColorThemeItem((int)Color.Gray, (int)Color.DarkRed) },
         };
 
+        private (float, float, float) RgbToHsv(byte r, byte g ,byte b)
+        {
+            // r,g,b values are from 0 to 1
+            // h = [0,360], s = [0,1], v = [0,1]
+            //  if s == 0, then h = -1 (undefined)
+            float h, s, v;
+
+            float _r = (float)r / (float)byte.MaxValue;
+            float _g = (float)g / (float)byte.MaxValue;
+            float _b = (float)b / (float)byte.MaxValue;
+
+            float min = Math.Min(Math.Min(_r, _g), _b);
+            float max = Math.Max(Math.Max(_r, _g), _b);
+            v = max;
+
+            float delta = max - min;
+            if (max != 0f)
+                s = delta / max;
+            else
+            {
+                s = 0f;
+                h = -1f;
+                return (h, s, v);
+            }
+
+            if (r == max)
+                h = (g - b) / delta;
+            else if(g == max)
+                h = 2f + (b - r) / delta;
+            else
+                h = 4f + (r - g) / delta;
+
+            h *= 60f;
+            if (h < 0f)
+                h += 360f;
+
+            return (h, s, v);
+        }
+
+        private (byte, byte, byte) HsvToRgb(float h, float s, float v)
+        {
+            float i;
+            byte r, g, b;
+            float f, p, q, t;
+            float _r, _g, _b;
+
+            if (s == 0)
+            {
+                byte _v = (byte) (v * byte.MaxValue);
+                r = g = b = _v;
+                return (r, g, b);
+            }
+
+            h /= 60f;
+            i = (float)Math.Floor(h);
+            f = h - i;
+            p = v * (1 - s);
+            q = v * (1 - s * f);
+            t = v * (1 - s * (1 - f));
+            switch (i)
+            {
+                case 0:
+                    _r = v;
+                    _g = t;
+                    _b = p;
+                    break;
+                case 1:
+                    _r = q;
+                    _g = v;
+                    _b = p;
+                    break;
+                case 2:
+                    _r = p;
+                    _g = v;
+                    _b = t;
+                    break;
+                case 3:
+                    _r = p;
+                    _g = q;
+                    _b = v;
+                    break;
+                case 4:
+                    _r = t;
+                    _g = p;
+                    _b = v;
+                    break;
+                default:        // case 5:
+                    _r = v;
+                    _g = p;
+                    _b = q;
+                    break;
+            }
+
+            r = (byte)(_r * byte.MaxValue);
+            g = (byte)(_g * byte.MaxValue);
+            b = (byte)(_b * byte.MaxValue);
+
+            return (r, g, b);
+        }
+
         private int mixSingleColor(int c1, int c2)
         {
             if (c1 == -1)
@@ -78,7 +178,16 @@ namespace PowerDir.themes
                 var (r1, g1, b1) = hexToRgb(c1);
                 var (r2, g2, b2) = hexToRgb(c2);
                 //return rgbToHex((byte)(r1 + r2 / 2), (byte)(g1 + g2 / 2), (byte)(b1 + b2 / 2));
-                return rgbToHex((byte)(r1 & r2), (byte)(g1 & g2), (byte)(b1 & b2));
+               
+                //return rgbToHex((byte)(r1 & r2), (byte)(g1 & g2), (byte)(b1 & b2));
+
+                // TODO try with HSL, HSV transforms
+
+                var (h1,s1, v1) = RgbToHsv(r1,g1,b1);
+                var (h2,s2, v2) = RgbToHsv(r2,g2,b2);
+
+                var (r,g,b) = HsvToRgb((h1 + h2), (s1 + s2) / 2f, (v1 + v2) / 2f);
+                return rgbToHex(r, g, b);
             }
         }
 
